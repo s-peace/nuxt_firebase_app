cf5f93f3f96f06df26ddbbfe56a2a3ce32d9edaa
s-peace <ssmim777@gmail.com>
2019-03-11 02:23:03 +0900

    serach orderby range

--- pages/axios.vue
+++ pages/axios.vue
@@ -4,8 +4,8 @@
     <p>{{message}}</p>
     <input v-model="find">
     <button @click="getData">find</button>
-    <ul>
-      <li>{{json_data}}</li>
+    <ul v-for="(data,key) in json_data">
+      <li><strong>{{key}}</strong><br>{{data}}</li>
     </ul>
   </section>
 </template>
@@ -13,7 +13,7 @@
 <script>
   const axios = require('axios');
 
-  let url = "https://nuxt-firebase-app-5b6cc.firebaseio.com/person.json?orderBy=%22$key%22&equalTo=%22";
+  let url = "https://nuxt-firebase-app-5b6cc.firebaseio.com/person.json?orderBy=%22age%22";
 
   export default {
     data: function(){
@@ -26,9 +26,11 @@
     },
     methods: {
       getData: function(){
-        let id_url = url + this.find + '%22';
-        axios.get(id_url).then((res) => {
-          this.message = 'get ID=' + this.find;
+        let range = this.find.split(',');
+        let age_url = url + "&startAt=" + range[0]
+          + "&endAt=" + range[1];
+        axios.get(age_url).then((res) => {
+          this.message = 'get: ' + range[0] + ' < age < ' + range[1];
           this.json_data = res.data;
       }).catch((error) => {
         this.message = 'ERROR';
